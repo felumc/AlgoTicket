243d470c63e702117fe7b9d8153c25d50b3f8f37
rrQuintana <113273490+rrQuintana@users.noreply.github.com>
2023-03-26 19:25:22 -0600

    Prueva de commit

--- config/db.config.js
+++ config/db.config.js
@@ -9,6 +9,6 @@ module.exports = {
       max: 5,
       min: 0,
       acquire: 30000, //tiempo máximo, en milisegundos, que el grupo intentará conectarse antes de lanzar el error
-      idle: 10000, //tiempo máximo, en milisegundos, que una conexión puede estar inactiva antes de ser liberada
+      idle: 10000, //tiempo máximo, en milisegundos, que una conexión puede estar inactiva antes de ser liberada.
     }
   };
