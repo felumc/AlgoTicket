db84b509a330c5b46a414f618c3af0e933a7199d
rrQuintana <113273490+rrQuintana@users.noreply.github.com>
2023-03-29 11:42:21 -0600

    Controlador boleto

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import Banner from './components/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner></Banner>
+    </div>
+  );
+}
+
+export default App;
